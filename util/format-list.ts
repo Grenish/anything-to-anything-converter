5b671c3a47a9a93853d5f707729aa0c4b87bcd04
Grenish rai <mrcoder2033d@gmail.com>
2025-12-27 00:44:54 +0530

    Create format-list.ts

---
+++ util/format-list.ts
@@ -0,0 +1,8 @@
+export const formats = [
+  { type: "document", values: ["pdf", "html", "markdown"] },
+  {
+    type: "data",
+    values: ["csv", "json", "toml", "yaml", "ini", "xml", "ndjson", "xlsx"],
+  },
+  { type: "image", values: ["png", "jpg", "jpeg", "webp"] },
+];
